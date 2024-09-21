e825c75ba47f9ecbc8ef8b8269e1717c2579dcd7
Roman Belyaev <rsbel94@ya.ru>
2024-09-21 15:06:29 +0300

    init commit

---
+++ src/pointersAndConst.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main()
+{
+    int var{ 0 };
+    const int* ciptr = &var;
+    int const* icptr = &var;
+    int* const iscptr = &var;
+    const int* const cptr = var;
+
+    ciptr++;
+    (*ciptr)++;
+
+    icptr++;
+    (*icptr)++;
+
+    iscptr++;
+    (*iscptr)++;
+
+    cptr++;
+    (*cptr)++;
+}
+
