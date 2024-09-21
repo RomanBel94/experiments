45296de542189d6944075520fb85f5430e595dbe
Roman Belyaev <rsbel94@ya.ru>
2024-09-21 15:49:21 +0300

    added findUniqueWithXOR.cpp

---
+++ src/findUniqueWithXOR.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+int findUniqueWithXOR(const std::vector<int>& vec)
+{
+    int result = 0;
+    for( auto num : vec )
+    {
+        result ^= num;
+    }
+    return result;
+}
+
+int main()
+{
+    std::vector<int> nums{ 2, 2, 1, 1, 3 };
+    std::cout << findUniqueWithXOR(nums) << std::endl;
+    return 0;
+}
