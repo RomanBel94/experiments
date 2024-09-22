b1750a85c971f45fd1e72767124624ac172e7b34
Roman Belyaev <rsbel94@ya.ru>
2024-09-22 13:48:55 +0300

    added myStrlen.c

---
+++ src/myStrlen.c
@@ -0,0 +1,19 @@
+#include <stddef.h>
+#include <stdio.h>
+
+size_t my_strlen(const char* str)
+{
+    const char* end = str;
+    while(*end) ++end;
+
+    return end - str;
+}
+
+int main()
+{
+    const char str[] = "Hello World";
+    printf("%s\n", str);
+    printf("%ld\n", my_strlen(str));
+
+    return 0;
+}
