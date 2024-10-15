dd84dbe8e1c318eb0a9aab134bba5d296edb7dc1
Roman Belyaev <rsbel94@ya.ru>
2024-10-15 16:30:17 +0300

    added inner_scope experinent

---
+++ src/inner_scope.cpp
@@ -0,0 +1,29 @@
+#include <ctime>
+#include <endian.h>
+#include <iostream>
+
+class A
+{
+public:
+    A() { std::cout << "A()\n"; }
+    ~A() { std::cout << "~A()\n"; }
+};
+
+class B
+{
+public:
+    B() { std::cout << "B()\n"; }
+    ~B() { std::cout << "~B()\n"; }
+};
+
+
+int main()
+{
+    A a;
+    {
+        B b;
+    }
+    A a2;
+
+    return 0;
+}
