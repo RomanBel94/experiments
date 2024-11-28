e492f494468db966a7bbcb72c0a2a88e3693abcd
Roman Belyaev <rsbel94@ya.ru>
2024-11-28 21:19:52 +0300

    added variadic_templates.cpp

---
+++ src/variadic_arguments.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+template<typename func, typename... Args>
+void functionCaller(func&& f, Args&& ... args)
+{
+    f(args...);
+}
+
+void printNum(int num)
+{
+    std::cout << num << std::endl;
+}
+
+void printSum(int a, int b)
+{
+    std::cout << a + b << std::endl;
+}
+
+void foo(int a, int b, int c, int d)
+{
+    std::cout << "abcd" << std::endl;
+}
+
+int main()
+{
+    functionCaller(printNum, 5);
+    functionCaller(printSum, 5, 6);
+    functionCaller(foo, 1, 2, 3, 4);
+
+    return 0;;
+}
