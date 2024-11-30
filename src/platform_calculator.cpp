f8e265795263ea0ae74c8ae88bc80ca07768eadb
Roman Belyaev <rsbel94@ya.ru>
2024-11-30 22:39:28 +0300

    added platform calculator

---
+++ src/platform_calculator.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+    double track;
+    std::cout << "Enter track number to know your platform: ";
+    std::cin >> track;
+
+    std::cout << "Your platform is: " << std::ceil(track / 2) << std::endl;
+
+    return 0;
+}
