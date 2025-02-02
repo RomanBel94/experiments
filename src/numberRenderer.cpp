597d7c01c74d409913aa65618d1d5412b98c7c7c
Roman Belyaev <rsbel94@ya.ru>
2025-02-02 13:00:47 +0300

    changed function for writing buffer

--- src/numberRenderer.cpp
+++ src/numberRenderer.cpp
@@ -3,139 +3,138 @@
 #include <string>
 #include <unordered_map>
 
-constexpr size_t HEIGHT = 9, WIDTH = 10;
+constexpr size_t HEIGHT = 9, WIDTH = 8;
 constexpr size_t BUFFER_HEIGHT = HEIGHT + 2;
 constexpr size_t BUFFER_WIDTH = WIDTH * 3 + 4;
 
 // clang-format off
 std::array<std::array<char, WIDTH>,HEIGHT> zero{
-    "   ###   ",
-    "  #   #  ",
-    " #     # ",
-    " #     # ",
-    " #     # ",
-    " #     # ",
-    " #     # ",
-    "  #   #  ",
-    "   ###   "
+    "  ###  ",
+    " #   # ",
+    "#     #",
+    "#     #",
+    "#     #",
+    "#     #",
+    "#     #",
+    " #   # ",
+    "  ###  "
 };
 
 std::array<std::array<char, WIDTH>,HEIGHT> one{
-    "    #    ",
-    "   ##    ",
-    "  # #    ",
-    " #  #    ",
-    "    #    ",
-    "    #    ",
-    "    #    ",
-    "    #    ",
-    "  #####  "
+    "   #   ",
+    "  ##   ",
+    " # #   ",
+    "#  #   ",
+    "   #   ",
+    "   #   ",
+    "   #   ",
+    "   #   ",
+    " ##### "
 };
 
 std::array<std::array<char, WIDTH>,HEIGHT> two{
-    "   ###   ",
-    "  #   #  ",
-    " #     # ",
-    "      #  ",
-    "     #   ",
-    "    #    ",
-    "   #     ",
-    "  #      ",
-    " ####### "
+    "  ###  ",
+    " #   # ",
+    "#     #",
+    "     # ",
+    "    #  ",
+    "   #   ",
+    "  #    ",
+    " #     ",
+    "#######"
 };
 
 std::array<std::array<char, WIDTH>,HEIGHT> three{
-    "  ####   ",
-    " #    #  ",
-    " #     # ",
-    "      #  ",
-    "     #   ",
-    "      #  ",
-    " #     # ",
-    " #    #  ",
-    "  ####   "
+    " ####  ",
+    "#    # ",
+    "#     #",
+    "     # ",
+    "    #  ",
+    "     # ",
+    "#     #",
+    "#    # ",
+    " ####  "
 };
 
 std::array<std::array<char, WIDTH>,HEIGHT> four{
-    "      #  ",
-    "     ##  ",
-    "    # #  ",
-    "   #  #  ",
-    "  #   #  ",
-    " ####### ",
-    "      #  ",
-    "      #  ",
-    "      #  "
+    "     # ",
+    "    ## ",
+    "   # # ",
+    "  #  # ",
+    " #   # ",
+    "#######",
+    "     # ",
+    "     # ",
+    "     # "
 };
 
 std::array<std::array<char, WIDTH>,HEIGHT> five{
-    " ####### ",
-    " #       ",
-    " #       ",
-    " #####   ",
-    " #    #  ",
-    "       # ",
-    "       # ",
-    " #    #  ",
-    "  ####   "
+    "#######",
+    "#      ",
+    "#      ",
+    "#####  ",
+    "#    # ",
+    "      #",
+    "      #",
+    "#    # ",
+    " ####  "
 };
 
 std::array<std::array<char, WIDTH>,HEIGHT> six{
-    "   ####  ",
-    "  #    # ",
-    " #       ",
-    " # ####  ",
-    " ##    # ",
-    " #     # ",
-    " #     # ",
-    "  #    # ",
-    "   ####  "
+    "  #### ",
+    " #    #",
+    "#      ",
+    "# #### ",
+    "##    #",
+    "#     #",
+    "#     #",
+    " #    #",
+    "  #### "
 };
 
 std::array<std::array<char, WIDTH>,HEIGHT> seven{
-    " ####### ",
-    "      #  ",
-    "     #   ",
-    "    #    ",
-    "    #    ",
-    "    #    ",
-    "    #    ",
-    "    #    ",
-    "    #    "
+    "#######",
+    "     # ",
+    "    #  ",
+    "   #   ",
+    "   #   ",
+    "   #   ",
+    "   #   ",
+    "   #   ",
+    "   #   "
 };
 
 std::array<std::array<char, WIDTH>,HEIGHT> eight{
-    "   ###   ",
-    "  #   #  ",
-    "  #   #  ",
-    "   ###   ",
-    "  #   #  ",
-    " #     # ",
-    " #     # ",
-    "  #   #  ",
-    "   ###   "
+    "  ###  ",
+    " #   # ",
+    " #   # ",
+    "  ###  ",
+    " #   # ",
+    "#     #",
+    "#     #",
+    " #   # ",
+    "  ###  "
 };
 
 std::array<std::array<char, WIDTH>,HEIGHT> nine{
-    "  ####   ",
-    " #    #  ",
-    " #     # ",
-    " #     # ",
-    " #    ## ",
-    "  #### # ",
-    "       # ",
-    " #    #  ",
-    "  ####   "
+    " ####  ",
+    "#    # ",
+    "#     #",
+    "#     #",
+    "#    ##",
+    " #### #",
+    "      #",
+    "#    # ",
+    " ####  "
 };
 
 // clang-format on
-void write_to_buffer(decltype(zero) digit,
-                     size_t position,
+void write_to_buffer(std::array<std::array<char, WIDTH>,HEIGHT> digit, size_t buffer_x, size_t buffer_y,
                      char buffer[BUFFER_HEIGHT][BUFFER_WIDTH])
 {
     for (size_t i = 0; i < HEIGHT; ++i)
         for (size_t j = 0; j < WIDTH; ++j)
-            buffer[i + 1][j + 1 + WIDTH * position] = digit[i][j];
+            buffer[buffer_y + i][buffer_x + j] = digit[i][j];
 }
 
 int main(int argc, char* argv[]) {
@@ -174,11 +173,12 @@ int main(int argc, char* argv[]) {
     char buffer[BUFFER_HEIGHT][BUFFER_WIDTH]{};
 
     for (size_t i = 0; i < 3; ++i) {
-        write_to_buffer(numbers[number[i]], i, buffer);
+        write_to_buffer(numbers[number[i]], (WIDTH + 1) * i + 1, 1,  buffer);
     }
 
     for (size_t i = 0; i < BUFFER_HEIGHT; ++i) {
-        for (size_t j = 0; j < BUFFER_WIDTH; ++j) std::cout << buffer[i][j];
+        for (size_t j = 0; j < BUFFER_WIDTH; ++j)
+            std::cout << buffer[i][j];
 
         std::cout << '\n';
     }
