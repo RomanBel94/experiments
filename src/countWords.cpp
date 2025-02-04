3d36ebb4b94bfc49807ef8a6180a431ad853c0f0
Roman Belyaev <rsbel94@ya.ru>
2025-02-04 20:11:07 +0300

    added countWords() overload

--- src/countWords.cpp
+++ src/countWords.cpp
@@ -1,15 +1,35 @@
+#include <cctype>
 #include <iostream>
 #include <sstream>
 #include <string>
 
-unsigned int countWords(std::string& str)
+unsigned int countWords(const std::string& str)
 {
+    std::cout << "countWords(const std::string& str)" << std::endl;
     std::istringstream stream(str);
     std::string word;
     unsigned int count{0};
 
     while (stream >> word)
         ++count;
+    return count;
+}
+
+unsigned int countWords(const std::string& str, bool notused)
+{
+    std::cout << "countWords(const std::string& str, bool notused)"
+              << std::endl;
+    auto it = str.begin();
+    unsigned int count{0};
+
+    while (it != str.end())
+    {
+        while (it != str.end() && std::isspace(*it))
+            ++it;
+        while (it != str.end() && !std::isspace(*it))
+            ++it;
+        ++count;
+    }
     return count;
 }
 
@@ -20,6 +40,9 @@ int main()
 
     std::cout << countWords(expr1) << std::endl;
     std::cout << countWords(expr2) << std::endl;
+
+    std::cout << countWords(expr1, true) << std::endl;
+    std::cout << countWords(expr2, true) << std::endl;
 
     return 0;
 }
