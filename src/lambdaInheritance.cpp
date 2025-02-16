30df61977fa1e7ae8a34f47c169421ba61c22caf
Roman Belyaev <rsbel94@ya.ru>
2025-02-16 20:41:06 +0300

    added lambdaInheritance

---
+++ src/lambdaInheritance.cpp
@@ -0,0 +1,31 @@
+#include <algorithm>
+#include <iomanip>
+#include <iostream>
+#include <string>
+#include <utility>
+#include <variant>
+#include <vector>
+template <typename... Lambdas>
+struct Visitor : Lambdas...
+{
+    Visitor(Lambdas... lambdas) : Lambdas(std::forward<Lambdas>(lambdas))... {}
+    using Lambdas::operator()...;
+};
+
+using var_t = std::variant<int, double, std::string>;
+
+int main()
+{
+    std::vector<var_t> vec = {10, 1.5, "hello"};
+    std::for_each(vec.begin(), vec.end(),
+                  [](const auto& v)
+                  {
+                      std::visit(
+                          Visitor{[](int arg) { std::cout << arg << ' '; },
+                                  [](double arg) { std::cout << arg << ' '; },
+                                  [](const std::string& arg)
+                                  { std::cout << std::quoted(arg) << ' '; }},
+                          v);
+                  });
+    return 0;
+}
