9b19bfe761d09a6272b7f1fc70cd1105023bf56d
Roman Belyaev <rsbel94@ya.ru>
2025-03-20 11:17:37 +0300

    started rewriting polynom parser

--- src/twoPolynomsAdding.cpp
+++ src/twoPolynomsAdding.cpp
@@ -1,60 +1,204 @@
 #include <fstream>
 #include <iostream>
+#include <list>
 #include <map>
 #include <string>
 
+namespace LexerParser
+{
+    enum class TokenType : unsigned char
+    {
+        Undefined,
+        Forbiden,
+        Number,
+        Newline,
+        Eof
+    };
+
+    // ------------------------------------------------------------------------
+
+    struct Token
+    {
+        const TokenType type;
+        const std::string value;
+
+        Token() = delete;
+        explicit Token(const TokenType type, const std::string& value = "") :
+            type(type), value(value) {}
+        ~Token() = default;
+
+    private:
+        Token(const Token&) = delete;
+        Token(Token&&) noexcept = delete;
+        const Token& Token::operator=(const Token&) = delete;
+        Token&& Token::operator=(Token&&) noexcept = delete;
+    };
+
+    // ------------------------------------------------------------------------
+
+    class Lexer
+    {
+    private:
+        TokenType current_token_type;
+        std::string current_token_value;
+        std::list<Token> tokens;
+        std::ifstream input_file;
+        char current_char;
+
+        void skip_spaces();
+        void read_token();
+        void define_token_type(const std::string& token);
+        void add_token();
+        void add_eof();
+        void reset_current_token();
+        bool is_number(const std::string& token);
+
+    public:
+        Lexer();
+        ~Lexer();
+
+        void parse();
+        inline const std::list<Token>& get_tokens() const;
+    };
+
+    Lexer::Lexer() : current_token_type(TokenType::Undefined), current_token_value(""), current_char('\0')
+    {
+        input_file.open("polynoms.txt", std::ios::in);
+        if (!input_file.is_open())
+            std::cout << "Can't open input file!" << std::endl;
+
+        current_char = input_file.get();
+    }
+
+    Lexer::~Lexer()
+    {
+        if (input_file.is_open())
+            input_file.close();
+    }
+
+    void Lexer::skip_spaces()
+    {
+        while (isspace(current_char) && current_char != '\n')
+            current_char = input_file.get();
+    }
+
+    void Lexer::read_token()
+    {
+        skip_spaces();
+        if (input_file.eof())
+        {
+            add_eof();
+            return;
+        }
+        if (current_char == '\n')
+            current_token_value += current_char;
+        else
+        {
+            while (!isspace(current_char) && !input_file.eof())
+            {
+                current_token_value += current_char;
+                current_char = input_file.get();
+            }
+        }
+        add_token();
+    }
+
+    void Lexer::define_token_type(const std::string& token)
+    {
+        if (token == "\n")
+            current_token_type = TokenType::Newline;
+        else if (is_number(token))
+            current_token_type = TokenType::Number;
+        else
+            current_token_type = TokenType::Forbiden;
+    }
+
+    void Lexer::add_token()
+    {
+        define_token_type(current_token_value);
+        tokens.emplace_back(current_token_type, current_token_value);
+        reset_current_token();
+    }
+
+    void Lexer::add_eof()
+    {
+        current_token_type = TokenType::Eof;
+        tokens.emplace_back(current_token_type);
+    }
+
+    void Lexer::reset_current_token()
+    {
+        if (current_token_type == TokenType::Newline)
+            current_char = input_file.get();
+
+        current_token_type = TokenType::Undefined;
+        current_token_value.clear();
+    }
+
+    bool Lexer::is_number(const std::string& token)
+    {
+        for (char symbol : token)
+            if (!isdigit(symbol))
+                return false;
+
+        return true;
+    }
+
+    void Lexer::parse()
+    {
+        if (!input_file.is_open())
+            return;
+
+        while (current_token_type != TokenType::Eof)
+            read_token();
+    }
+
+    inline const std::list<Token>& Lexer::get_tokens() const
+    {
+        return tokens;
+    }
+
+    // ------------------------------------------------------------------------
+
+    class Parser
+    {
+    private:
+        Lexer lexer;
+    };
+};
+
 int main()
 {
-    std::ifstream input_file("polynoms.txt", std::ios::in);
+    LexerParser::Lexer lexer;
+    lexer.parse();
+    size_t token_number{ 0 };
 
-    if (!input_file.is_open())
+    for (const LexerParser::Token& token : lexer.get_tokens())
     {
-        std::cout << "Can't open input file!" << std::endl;
-        return -1;
-    }
-
-    std::map<int, int> first_polynom{};
-    std::map<int, int> second_polynom{};
-    std::map<int, int> result_polynom{};
-    std::map<int, int>* current_polynom = &first_polynom;
-
-    std::string number_buffer;
-    std::string power_buffer;
-
-    while (!input_file.eof())
-    {
-        // go to first number
-        while (std::isspace(input_file.peek()))
-            input_file.get();
-        // read first number
-        while (std::isdigit(input_file.peek()))
-            power_buffer += input_file.get();
-        // go to second number
-        while (std::isspace(input_file.peek()))
-            input_file.get();
-        // read second number
-        while (std::isdigit(input_file.peek()))
-            number_buffer += input_file.get();
-        // go to end of line
-        while (std::isspace(input_file.peek()) && input_file.peek() != '\n')
-            input_file.get();
-
-        current_polynom->insert(
-            {std::stoi(number_buffer), std::stoi(power_buffer)});
-        number_buffer.clear();
-        power_buffer.clear();
-
-        if (input_file.get() == '\n' && input_file.peek() == '\n')
-            current_polynom = &second_polynom;
-    }
-
-    int iterations = std::max(first_polynom.size(), second_polynom.size());
-
-    // adding polynoms
-    for (int i = 0; i <= iterations; ++i)
-    {
-        result_polynom[i] = first_polynom[i] + second_polynom[i];
-        std::cout << "Number " << result_polynom[i] << " power " << i << '\n';
+        switch (token.type)
+        {
+        case LexerParser::TokenType::Undefined:
+            std::cout << token_number << ". Token type: Undefined; "
+                << "token value: " << token.value << '\n';
+            break;
+        case LexerParser::TokenType::Forbiden:
+            std::cout << token_number << ". Token type: Forbiden; "
+                << "token value: " << token.value << '\n';
+            break;
+        case LexerParser::TokenType::Newline:
+            std::cout << token_number << ". Token type: Newline; "
+                << "token value: \\n\n";
+            break;
+        case LexerParser::TokenType::Number:
+            std::cout << token_number << ". Token type: Number; "
+                << "token value: " << token.value << '\n';
+            break;
+        case LexerParser::TokenType::Eof:
+            std::cout << token_number << ". Token type: Eof; "
+                << "token value: eof()";
+            break;
+        }
+        ++token_number;
     }
 
     return 0;
