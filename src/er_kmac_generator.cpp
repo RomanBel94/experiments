3f272deb21aea174c2fef43391d46a5e7b90d510
Roman Belyaev <rsbel94@ya.ru>
2025-09-22 13:19:07 +0300

    added timestamp to the output

--- src/er_kmac_generator.cpp
+++ src/er_kmac_generator.cpp
@@ -1,15 +1,31 @@
 #include <cstdlib>
+#include <ctime>
 #include <fstream>
+#include <iomanip>
 #include <iostream>
+#include <memory>
 #include <random>
 #include <string>
 
 static constexpr std::size_t HASH_SIZE{48};
+static constexpr std::size_t FILENAME_SIZE{12};
 static const std::string digits{"123456789ABCDEF"};
+constexpr char* const TIME_FORMAT = {"%d.%m.%Y %T "};
+constexpr char* const COMPLETE_MSG = {"Generation completed: "};
 
 static std::random_device rd{};
 static const std::mt19937 gen(rd());
 static std::uniform_int_distribution<int> distribution(0, digits.size() - 1);
+
+static std::ostream& timestamp(std::ostream& stream)
+{
+    auto raw_time = std::time(nullptr);
+    auto tm_struct = *std::localtime(&raw_time);
+    const auto time{std::make_unique<tm>(tm_struct)};
+
+    stream << std::put_time(time.get(), TIME_FORMAT);
+    return stream;
+}
 
 static inline bool _is_hash(const std::string& hash)
 {
@@ -31,16 +47,16 @@ static inline void _fill_hash(std::string& hash)
         hash.push_back(digits[distribution(rd)]);
 }
 
-static inline void _write_hash(const std::string& result)
+static inline void _write_hash(const std::string& hash)
 {
-    std::ofstream output_file{result + ".txt", std::ios::out};
+    std::ofstream output_file{hash.substr(0, FILENAME_SIZE) + ".txt",
+                              std::ios::out};
     if (!output_file)
-        std::cerr << "[ERROR] Failed to write output file\n";
+        std::cerr << "Failed to write output file\n";
     else
-        output_file << "[SUCCESS] Hash generation completed: " << result
-                    << "\n";
+        output_file << timestamp << COMPLETE_MSG << hash << "\n";
 
-    std::clog << "[SUCCESS] Hash generation completed: " << result << "\n";
+    std::clog << timestamp << COMPLETE_MSG << hash << "\n";
 }
 
 int main(int, char**)
@@ -52,7 +68,7 @@ int main(int, char**)
 
     if (!_is_valid_hash(result))
     {
-        std::cerr << "[ERROR] Failed to generate hashkey\n";
+        std::cerr << "Failed to generate\n";
         std::system("pause");
         std::exit(EXIT_FAILURE);
     }
