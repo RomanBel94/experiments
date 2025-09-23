316bb529684f11a56c7b6d6d9a649a24ae99640e
Roman Belyaev <rsbel94@ya.ru>
2025-09-23 19:58:30 +0300

    removed _is_valid_hash() function

--- src/er_kmac_generator.cpp
+++ src/er_kmac_generator.cpp
@@ -13,7 +13,7 @@ static std::random_device rd{};
 static const std::mt19937 gen(rd());
 static std::uniform_int_distribution<int> distribution(0, digits.size() - 1);
 
-static inline bool _is_hash(const std::string& hash)
+static inline bool _is_hash(const std::string& hash) noexcept
 {
     for (auto ch : hash)
         if (!std::isxdigit(ch))
@@ -22,18 +22,13 @@ static inline bool _is_hash(const std::string& hash)
     return true;
 }
 
-static inline bool _is_valid_hash(const std::string& hash)
-{
-    return _is_hash(hash) && hash.size() == HASH_SIZE;
-}
-
-static inline void _fill_hash(std::string& hash)
+static inline void _fill_hash(std::string& hash) noexcept
 {
     for (std::size_t i{0}; i < HASH_SIZE; ++i)
         hash.push_back(digits[distribution(rd)]);
 }
 
-static inline void _write_hash(const std::string& hash)
+static inline void _write_hash(const std::string& hash) noexcept
 {
     std::ofstream output_file{hash.substr(0, FILENAME_SIZE) + ".txt",
                               std::ios::out};
@@ -56,7 +51,7 @@ int main(int, char**)
 
     _fill_hash(result);
 
-    if (!_is_valid_hash(result))
+    if (!_is_hash(result) || result.size() != HASH_SIZE)
     {
         std::cerr << "Failed to generate\n";
         std::exit(EXIT_FAILURE);
