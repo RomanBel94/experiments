9a0362b4b3639b860aa00756b1555580092b42e4
Roman Belyaev <rsbel94@ya.ru>
2025-10-08 12:45:07 +0300

    made utf-8 output

--- src/hashgen.cpp
+++ src/hashgen.cpp
@@ -1,7 +1,6 @@
 #include <fstream>
 #include <iostream>
 #include <random>
-#include <stdexcept>
 #include <string>
 
 class SimpleHashGenerator
@@ -10,31 +9,32 @@ public:
     SimpleHashGenerator() = default;
     ~SimpleHashGenerator() noexcept = default;
 
-    std::string get_hash(std::size_t size);
+    std::wstring get_hash(std::size_t size);
 
 private:
-    static const std::string digits;
+    static const std::wstring digits;
 
     static std::random_device rd;
     static const std::mt19937 gen;
     static std::uniform_int_distribution<int> distribution;
 
-    static void _fill_hash(std::string& hash, std::size_t amount) noexcept;
+    static void _fill_hash(std::wstring& hash,
+                           const std::size_t amount) noexcept;
 };
 
-std::string SimpleHashGenerator::get_hash(std::size_t size)
+std::wstring SimpleHashGenerator::get_hash(const std::size_t size)
 {
     if (size > 0)
     {
-        std::string result;
+        std::wstring result;
         _fill_hash(result, size);
         return result;
     }
-    else
-        throw std::runtime_error{"null hash size"};
+
+    return L"";
 }
 
-const std::string SimpleHashGenerator::digits{"123456789ABCDEF"};
+const std::wstring SimpleHashGenerator::digits{L"123456789ABCDEF"};
 
 std::random_device SimpleHashGenerator::rd{};
 const std::mt19937 SimpleHashGenerator::gen(rd());
@@ -42,8 +42,8 @@ std::uniform_int_distribution<int>
     SimpleHashGenerator::distribution(0, digits.size() - 1);
 
 // Clears parameter and fills it with passed amount
-inline void SimpleHashGenerator::_fill_hash(std::string& hash,
-                                            std::size_t amount) noexcept
+void SimpleHashGenerator::_fill_hash(std::wstring& hash,
+                                     const std::size_t amount) noexcept
 {
     hash.clear();
     hash.reserve(amount);
@@ -52,36 +52,38 @@ inline void SimpleHashGenerator::_fill_hash(std::string& hash,
         hash.push_back(digits[distribution(rd)]);
 }
 
-int main(int argc, char** argv)
+static int wmain(int argc, wchar_t** argv)
 {
+#ifdef _WIN32
+    std::setlocale(LC_ALL, "ru_RU.UTF-8");
+#endif // _WIN32
+
     if (argc != 2)
     {
-        std::cout << "usage: hashgen hash_size\n";
+        std::wcout << L"Использование: hashgen <размер хеша>\n";
+        std::exit(EXIT_FAILURE);
+    }
+    if (std::stoi(argv[1]) <= 0)
+    {
+        std::wcerr << L"Размер хеша должен быть больше 0\n";
         std::exit(EXIT_FAILURE);
     }
 
     SimpleHashGenerator gen;
-    std::string res;
-    try
-    {
-        res = gen.get_hash(std::stoi(argv[1]));
-    }
-    catch (const std::exception& ex)
-    {
-        std::cerr << ex.what() << std::endl;
-        std::exit(EXIT_FAILURE);
-    }
+    std::wstring hash;
 
-    std::size_t filename_size = res.size() > 10 ? 10 : res.size();
-    std::ofstream output_file(res.substr(0, filename_size) + ".txt",
-                              std::ios::out);
+    hash = gen.get_hash(std::stoi(argv[1]));
+
+    std::size_t filename_size = hash.size() >= 10 ? 10 : hash.size();
+    std::wofstream output_file(hash.substr(0, filename_size) + L".txt",
+                               std::ios::out);
     if (!output_file)
     {
-        std::cout << "Can't write file\n";
+        std::wcerr << L"Не удалось записать файл\n";
         std::exit(EXIT_FAILURE);
     }
-    output_file << res;
-    std::cout << "done\n";
+    output_file << hash;
+    std::wcout << L"Готово\n";
 
     return EXIT_SUCCESS;
 }
