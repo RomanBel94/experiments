65e7f8ed8130869b7ba08afdd19394b552dbf9cb
Roman Belyaev <rsbel94@ya.ru>
2025-11-09 14:44:34 +0300

    blinking_timer: made timer as templated class

--- src/blink_timer.cpp
+++ src/blink_timer.cpp
@@ -3,30 +3,25 @@
 #include <iostream>
 #include <ratio>
 
+template <std::size_t pos_phase, std::size_t neg_phase>
 class blink_timer final
 {
 private:
-    blink_timer() = delete;
     blink_timer(const blink_timer&) = delete;
     blink_timer(blink_timer&&) noexcept = delete;
     blink_timer& operator=(const blink_timer&) = delete;
     blink_timer&& operator=(blink_timer&&) noexcept = delete;
 
-    std::chrono::duration<std::size_t, std::milli> _positive_phase{};
-    std::chrono::duration<std::size_t, std::milli> _negative_phase{};
-    std::chrono::duration<std::size_t, std::milli> _full_cycle{};
+    const std::chrono::duration<std::size_t, std::milli> _positive_phase{
+        pos_phase};
+    const std::chrono::duration<std::size_t, std::milli> _negative_phase{
+        neg_phase};
 
-    bool _is_active{false};
-
-    std::chrono::time_point<std::chrono::high_resolution_clock> start_time;
+    const std::chrono::time_point<std::chrono::high_resolution_clock>
+        start_time{};
 
 public:
-    blink_timer(std::size_t positive_phase, std::size_t negative_phase)
-        : _positive_phase(positive_phase), _negative_phase(negative_phase),
-          _full_cycle(_positive_phase + _negative_phase)
-    {
-        start_time = std::chrono::high_resolution_clock::now();
-    }
+    blink_timer() : start_time(std::chrono::high_resolution_clock::now()) {}
 
     ~blink_timer() noexcept = default;
 
@@ -34,19 +29,18 @@ public:
     {
         auto current_time = std::chrono::high_resolution_clock::now();
         auto time_passed = current_time - start_time;
-        std::size_t num_cycles = time_passed / _full_cycle;
+        auto full_cycle = _positive_phase + _negative_phase;
+        std::size_t num_cycles = time_passed / full_cycle;
 
-        return ((time_passed - num_cycles * _full_cycle) < _positive_phase);
-        ;
+        return ((time_passed - num_cycles * full_cycle) < _positive_phase);
     }
 };
 
 int main()
 {
-    blink_timer timer(500, 500);
+    blink_timer<500, 500> timer;
 
+    std::cout << std::boolalpha;
     while (true)
-    {
-        std::cout << std::boolalpha << timer.current_phase() << '\n';
-    }
+        std::cout << timer.current_phase() << '\n';
 }
