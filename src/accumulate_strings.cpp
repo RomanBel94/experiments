a9872cb677bc1aff1948cb47fa74d06e40b4af50
Roman Belyaev <rsbel94@ya.ru>
2025-12-06 15:41:56 +0300

    accumulate_strings: made code prettier

--- src/accumulate_strings.cpp
+++ src/accumulate_strings.cpp
@@ -8,16 +8,15 @@
 int main()
 {
     std::array<std::string, 3> strings{"Hello ", "World", "!"};
-    std::string result =
-        std::accumulate(strings.cbegin(), strings.cend(), std::string(),
-                        std::plus<std::string>());
+    std::string result = std::accumulate(strings.cbegin(), strings.cend(), "",
+                                         std::plus<std::string>());
 
     std::cout << result << std::endl;
 
     std::array<std::string, 5> strings2{"Hello", "world", "one", "two",
                                         "three"};
     std::string result2 =
-        std::accumulate(strings2.cbegin(), strings2.cend(), std::string(),
+        std::accumulate(strings2.cbegin(), strings2.cend(), "",
                         [](const std::string& lhs, const std::string& rhs)
                         { return lhs + rhs + " "; });
 
