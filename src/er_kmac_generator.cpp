e1c110d2d4c94982b05254856a0a683d688fae49
Roman Belyaev <rsbel94@ya.ru>
2025-12-22 15:30:12 +0300

    er_kmac_generator: optimizations

--- src/er_kmac_generator.cpp
+++ src/er_kmac_generator.cpp
@@ -2,30 +2,32 @@
 #include <iostream>
 #include <random>
 #include <string>
+#include <string_view>
 
 static constexpr std::size_t HASH_SIZE{48};
 static constexpr std::size_t FILENAME_SIZE{16};
-static const std::string digits{"123456789ABCDEF"};
+static constexpr std::string_view digits{"123456789ABCDEF"};
 
 static std::random_device rd{};
 static const std::mt19937 gen(rd());
-static std::uniform_int_distribution<int> distribution(0, digits.size() - 1);
+static std::uniform_int_distribution<std::size_t>
+    distribution(0ull, digits.size() - 1ull);
 
 // Clears parameter and fills it with passed amount
-static inline void _fill_hash(std::string& hash,
-                              const std::size_t amount = 0) noexcept
+static inline void fill_hash(std::string& input,
+                             const std::size_t amount = 0ull) noexcept
 {
-    hash.clear();
-    hash.reserve(HASH_SIZE);
+    input.clear();
+    input.reserve(HASH_SIZE);
 
     for (std::size_t i{0}; i < amount; ++i)
-        hash.push_back(digits[distribution(rd)]);
+        input.push_back(digits[distribution(rd)]);
 }
 
 // Writes string into file
-static inline void _write_hash_to_file(const std::string& hash) noexcept
+static inline void write_hash_to_file(const std::string& hash) noexcept
 {
-    std::ofstream output_file{hash.substr(0, FILENAME_SIZE) + ".txt",
+    std::ofstream output_file{hash.substr(0ull, FILENAME_SIZE) + ".txt",
                               std::ios::out};
     if (!output_file)
     {
@@ -43,8 +45,8 @@ int main(int, char**)
 {
     std::string result{};
 
-    _fill_hash(result, HASH_SIZE);
-    _write_hash_to_file(result);
+    fill_hash(result, HASH_SIZE);
+    write_hash_to_file(result);
 
     return EXIT_SUCCESS;
 }
