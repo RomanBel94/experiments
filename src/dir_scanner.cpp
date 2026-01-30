cc1702566803b813f46df88c9a315acd8c33918a
Roman Belyaev <rsbel94@ya.ru>
2026-01-30 23:21:35 +0300

    dir_scanner: added passing directory through CLI argument

--- src/dir_scanner.cpp
+++ src/dir_scanner.cpp
@@ -1,17 +1,31 @@
 #include <cstdlib>
+#include <exception>
 #include <filesystem>
 #include <format>
 #include <iostream>
 
 namespace fs = std::filesystem;
 
-int main(int, char**)
+int main(int argc, char** argv)
 {
-    fs::path current_dir = fs::current_path();
+    fs::path current_dir;
 
-    for (const auto& file : fs::recursive_directory_iterator(current_dir))
+    if (argc == 1)
+        current_dir = fs::current_path();
+    else
+        current_dir = argv[1];
+
+    try
     {
-        std::cout << std::format("{}\n", file.path().string());
+        for (const auto& file : fs::recursive_directory_iterator(current_dir))
+        {
+            std::cout << std::format("{}\n", file.path().string());
+        }
+    }
+    catch (const std::exception& ex)
+    {
+        std::cerr << ex.what() << '\n';
+        return EXIT_FAILURE;
     }
 
     return EXIT_SUCCESS;
