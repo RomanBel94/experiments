04d466adedd902e33f14b0ee68e0fb2b169b0540
Roman Belyaev <rsbel94@ya.ru>
2026-01-30 23:44:32 +0300

    dir_scanner: added multiple paths passing through CLI

--- src/dir_scanner.cpp
+++ src/dir_scanner.cpp
@@ -1,26 +1,36 @@
+#include <cstddef>
 #include <cstdlib>
 #include <exception>
 #include <filesystem>
 #include <format>
 #include <iostream>
+#include <vector>
 
 namespace fs = std::filesystem;
+
+inline void recursive_print_directory(const fs::path& path)
+{
+    for (const auto& file : fs::recursive_directory_iterator(path))
+    {
+        std::cout << std::format("{}\n", file.path().string());
+    }
+}
 
 int main(int argc, char** argv)
 {
     fs::path current_dir;
+    std::vector<fs::path> paths;
 
     if (argc == 1)
         current_dir = fs::current_path();
     else
-        current_dir = argv[1];
+        for (std::size_t i = 1; i < argc; ++i)
+            paths.emplace_back(argv[i]);
 
     try
     {
-        for (const auto& file : fs::recursive_directory_iterator(current_dir))
-        {
-            std::cout << std::format("{}\n", file.path().string());
-        }
+        for (const auto& path : paths)
+            recursive_print_directory(path);
     }
     catch (const std::exception& ex)
     {
